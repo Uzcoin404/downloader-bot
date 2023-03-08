42cf4699acd75ab344ac2aa480f4c68b997eb7d0
Your Name <uzcointg@gmail.com>
2023-03-08 22:51:05 +0500

    some changes

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
 date_default_timezone_set('Asia/Tashkent');
-$siteUrl = 'https://8f3e-91-192-66-164.ngrok.io/shop-bot/';
+$siteUrl = 'https://e9be-213-230-74-166.eu.ngrok.io/telegram/downloader-bot/';
 
 require_once('library/Telegram.php');
 require_once('components/db.php');
 require_once('functions.php');
 require_once('user.php');
-include_once('components/products.php');
-include_once('components/brands.php');
-include_once('components/items.php');
-include_once('components/basket.php');
 
 $telegram = new Telegram("6197988281:AAHRmDRfpvSaj5oazfm4c3Vrz_8_iyye3Ho", true);
 $chatID = $telegram ->ChatID();
@@ -27,7 +23,37 @@ $fullName = $firstName . ' ' . $lastName;
 $username = $telegram -> Username();
 
 if ($text == '/start') {
-    
+    $func->sendMessage("Salom");
 } else {
-    
+    if (str_contains($text, "tiktok.com") || str_contains($text, "vm.tiktok.com")) {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://tiktok-download-without-watermark.p.rapidapi.com/analysis?url=$text&hd=0",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => [
+                "X-RapidAPI-Host: tiktok-download-without-watermark.p.rapidapi.com",
+                "X-RapidAPI-Key: dcb55c740emsh743e1fe2970921ep145c41jsn074a39d651d1"
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $data = json_decode($response);
+            var_dump($data);
+            $telegram->sendVideo(['chat_id' => $chatID, 'video' => $data->data->play, 'caption' => $data->data->title]);
+        }
+    }
 }
--- test.php
+++ test.php
@@ -1,12 +1,33 @@
 <?php
-include_once('components/db.php');
-$basket = [
-    'funds' => 0,
-    'items' => []
-];
-// echo json_encode($basket);
-$db = new Database();
-// var_dump(count(json_decode($db->getBasket(829349149)['basket'], true)['items']));
-var_dump(json_encode($basket));
-// var_dump(json_decode('{"cash":"0","basket":{"funds":0,"items":[{"id":"2","brand_id":"1","product_id":"1"}]}}'));
-?>
+include_once('./library/Telegram.php');
+$telegram = new Telegram("6197988281:AAHRmDRfpvSaj5oazfm4c3Vrz_8_iyye3Ho", true);
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+    CURLOPT_URL => "https://tiktok-download-without-watermark.p.rapidapi.com/analysis?url=https://www.tiktok.com/@the.nick.pro/video/6904271117783928065?lang=en.&hd=0",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => [
+        "X-RapidAPI-Host: tiktok-download-without-watermark.p.rapidapi.com",
+        "X-RapidAPI-Key: dcb55c740emsh743e1fe2970921ep145c41jsn074a39d651d1"
+    ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    $data = json_decode($response);
+    var_dump($data);
+    $telegram->sendVideo(['chat_id' => 829349149, 'video' => $data->data->play, 'caption' => $data->data->title]);
+}
